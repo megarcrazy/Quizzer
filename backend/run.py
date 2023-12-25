b43ae937345d270f3b6c700c808698b6194f02d2
megarcrazy <strangernotjoke@hotmail.com>
2023-12-25 14:33:21 +1100

    Removed run.py comments

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,5 @@ from app import app
 
 
 if __name__ == '__main__':
-    # # Insert sample values to the testing SQLite database
-    # insert_sample_quizzes()
-
     # Run the Flask backend application
     app.run(debug=True)
